0a1ecd2f919cc3fe15a0896ec3c9e358ceb50d1b
calaca-9ruby-ay2526 <calaca.mm@obmontessori.edu.ph>
2026-02-20 13:52:24 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function Discount() {
+    let budgetforlaptop = document.getElementById("Budget").value;
+    let discount = Number(budgetforlaptop * .2).toFixed(2);
+
+    document.getElementById().innerHTML = '
+    <div
+    
+    ';
+}
